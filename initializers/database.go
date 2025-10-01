4a14316f664ccc296cd0534d8a1977b3d0c0009a
aloneen <seisenbek2005@mail.ru>
2025-10-01 22:21:12 +0500

    Tested connection. Pooling configured

--- initializers/database.go
+++ initializers/database.go
@@ -3,6 +3,7 @@ package initializers
 import (
 	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -18,6 +19,15 @@ func ConnectToDatabase() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatal("Failed to get sql.DB:", err)
+	}
+
+	sqlDB.SetMaxOpenConns(10)
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	log.Println("Database connected")
 
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import "github.com/aloneen/assign2-ex2-adv/initializers"
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDatabase()
+}
 func main() {
 
 }

3f71b5d281c7bbfaa7e0af656d94d2d2633fda01
aloneen <seisenbek2005@mail.ru>
2025-10-01 22:13:27 +0500

    Initializers are created. Env files and Database connection

---
+++ initializers/database.go
@@ -0,0 +1,24 @@
+package initializers
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDatabase() {
+	dsn := os.Getenv("DATABASE_URL")
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Database connected")
+
+}
